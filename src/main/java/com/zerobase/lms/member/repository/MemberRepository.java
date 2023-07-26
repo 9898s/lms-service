91abc15ba71f7f49eb6ba297702b7c119a669998
9898s <sueh9898@icloud.com>
2023-07-26 19:35:19 +0900

    회원가입 및 인증메일

---
+++ src/main/java/com/zerobase/lms/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.zerobase.lms.member.repository;
+
+import com.zerobase.lms.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+    Optional<Member> findByEmailAuthKey(String emailAuthKey);
+}
