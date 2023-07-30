dfa3c5a0b97b944f16b19fbd2e09bc8f8d33ed59
9898s <sueh9898@icloud.com>
2023-07-31 01:57:00 +0900

    강좌 관리 - 강좌별 강좌 신청 내역

--- src/main/java/com/zerobase/lms/course/controller/AdminTakeCourseController.java
+++ src/main/java/com/zerobase/lms/course/controller/AdminTakeCourseController.java
@@ -1,13 +1,16 @@
 package com.zerobase.lms.course.controller;
 
+import com.zerobase.lms.course.dto.CourseDto;
 import com.zerobase.lms.course.dto.TakeCourseDto;
 import com.zerobase.lms.course.model.ServiceResult;
 import com.zerobase.lms.course.model.TakeCourseParam;
+import com.zerobase.lms.course.service.CourseService;
 import com.zerobase.lms.course.service.TakeCourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.CollectionUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -16,25 +19,29 @@ import java.util.List;
 @RequiredArgsConstructor
 @Controller
 public class AdminTakeCourseController extends BaseController {
+    private final CourseService courseService;
     private final TakeCourseService takeCourseService;
 
     @GetMapping("/admin/takecourse/list.do")
-    public String list(Model model, TakeCourseParam parameter) {
+    public String list(Model model, TakeCourseParam parameter, BindingResult bindingResult) {
         parameter.init();
-        List<TakeCourseDto> courseList = takeCourseService.list(parameter);
+        List<TakeCourseDto> list = takeCourseService.list(parameter);
 
         long totalCount = 0;
 
-        if (!CollectionUtils.isEmpty(courseList)) {
-            totalCount = courseList.get(0).getTotalCount();
+        if (!CollectionUtils.isEmpty(list)) {
+            totalCount = list.get(0).getTotalCount();
         }
 
         String queryString = parameter.getQueryString();
         String pagerHtml = getPagerHtml(totalCount, parameter.getPageSize(), parameter.getPageIndex(), queryString);
 
-        model.addAttribute("list", courseList);
+        model.addAttribute("list", list);
         model.addAttribute("totalCount", totalCount);
         model.addAttribute("pager", pagerHtml);
+
+        List<CourseDto> courseList = courseService.listAll();
+        model.addAttribute("courseList", courseList);
         return "admin/takecourse/list";
     }
 
